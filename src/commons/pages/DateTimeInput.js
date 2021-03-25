836dae6140ea961494a39432829840c3e54a4fa8
unknown <lgun202@naver.com>
2021-03-25 20:07:28 +0900

    [feat] 날짜별 시간 리스트 State 저장 및 불러오기: react Hooks의 setState는 비동기로 작동이 되기때문에 selectDay를 했을 때 한박자 계속 늦게 반영이됨. 따라서 useEffect를 사용해서 이를 해결해줘야함.

--- src/commons/pages/DateTimeInput.js
+++ src/commons/pages/DateTimeInput.js
@@ -1,32 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../assets/css/DatePicker.css"
+import '../../assets/css/Timepicker.css';
 import { Calendar } from "react-modern-calendar-datepicker";
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+
+
+const minimumDate = {
+  year: 2021,
+  month: 3,
+  day: 9
+};
+
+const maximumDate = {
+  year: 2021,
+  month: 12,
+  day: 17
+};
+
+const list = [];
+const minimumTime = 4;
+const maximumTime = 8;
+
+let initSelectedList = []
+for (let i = 0; i < 24; i++) {
+  let input = { name: i }
+  list.push(input)
+  if (minimumTime <= i && i <= maximumTime) {
+    initSelectedList.push(-1);
+  } else {
+    initSelectedList.push(0);
+  }
+};
+
+let availableDateTime = {};
 
 const DateTimeInput = (props) => {
 
-  const minimumDate = {
-    year: 2021,
-    month: 2,
-    day: 9
-  };
-
-  const maximumDate = {
-    year: 2021,
-    month: 2,
-    day: 17
-  };
-
   const [selectedDay, setSelectedDay] = useState(minimumDate);
+  const [selectedTime, setSelectedTime] = useState('');
+  const [selectedList, setSelectedList] = useState(initSelectedList);
+
+
+
+  const MenuItem = ({ text, isSelected }) => {
+    return <div
+      className={`menu-item ${isSelected === 1 ? 'active' : (isSelected === 0 ? 'inable' : '')}`}
+    >{text}</div>;
+  };
+
+  const Menu = (list) =>
+    list.map((el, index) => {
+      const { name } = el;
+      return <MenuItem text={name} key={name} isSelected={selectedList[index]} />;
+    });
+
+
+  const menu = Menu(list, selectedTime, selectedList);
+
+  const onSelectTime = key => {
+    setSelectedTime(key);
+    let newSelectedList = [...selectedList];
+    newSelectedList[key] *= -1;
+    setSelectedList(newSelectedList);
+    availableDateTime[`${selectedDay.year}-${selectedDay.month}-${selectedDay.day}`] = [...selectedList];
+  }
+
+  const onSelectedDay = day => {
+    setSelectedDay(day);
+  }
+
+  useEffect(() => {
+    if(`${selectedDay.year}-${selectedDay.month}-${selectedDay.day}` in availableDateTime){
+      setSelectedList(availableDateTime[`${selectedDay.year}-${selectedDay.month}-${selectedDay.day}`])
+    }
+    else{
+      setSelectedList(initSelectedList);
+    }
+  }, [selectedDay]);
+
+  
   return (
     <div>
-      <Calendar
-        value={selectedDay}
-        onChange={setSelectedDay}
-        colorPrimary="#8ec5ff"
-        minimumDate={minimumDate}
-        maximumDate={maximumDate}
-      />
+      <div>
+        <Calendar
+          value={selectedDay}
+          onChange={onSelectedDay}
+          colorPrimary="#8ec5ff"
+          minimumDate={minimumDate}
+          maximumDate={maximumDate}
+        />
+      </div>
+      <div className="Timepicker">
+        <p>시간 선택 - {selectedDay.month}월 {selectedDay.day}일</p>
+        <ScrollMenu
+          data={menu}
+          selected={selectedTime}
+          onSelect={onSelectTime}
+        />
+      </div>
     </div>
+
   );
 };
 
