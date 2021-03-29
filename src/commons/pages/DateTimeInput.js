5086331f19518f65bee0d8ddef964ebd6eb0001f
unknown <lgun202@naver.com>
2021-03-30 01:43:30 +0900

    [feat] 모두 선택할래요 기능 추가

--- src/commons/pages/DateTimeInput.js
+++ src/commons/pages/DateTimeInput.js
@@ -6,28 +6,35 @@ import { Link } from 'react-router-dom';
 
 const minimumDate = {
   year: 2021,
-  month: 3,
-  day: 9
+  month: 4,
+  day: 1
 };
 
 const maximumDate = {
   year: 2021,
-  month: 12,
+  month: 4,
   day: 17
 };
 
 const list = [];
-const minimumTime = 4;
-const maximumTime = 8;
+const minimumTime = 18;
+const maximumTime = 21;
+
+let TimePickerTranslate = -1;
+if(minimumTime != 0 && minimumTime <=14) TimePickerTranslate = -43 * minimumTime + 20;
+else if(14 < minimumTime) TimePickerTranslate = 14 * -43 + 20;
 
 let initSelectedList = []
+let allCheckedList = []
 for (let i = 0; i < 24; i++) {
   let input = { name: i }
   list.push(input)
   if (minimumTime <= i && i <= maximumTime) {
     initSelectedList.push(-1);
+    allCheckedList.push(1);
   } else {
     initSelectedList.push(0);
+    allCheckedList.push(0);
   }
 };
 
@@ -35,11 +42,26 @@ let availableDateTime = {};
 let availableDay = [];
 
 const DateTimeInput = ({ match }) => {
-
   const [selectedDay, setSelectedDay] = useState(minimumDate);
   const [selectedTime, setSelectedTime] = useState('');
   const [selectedList, setSelectedList] = useState(initSelectedList);
   const [selectFlag, setSelectFlag] = useState(1);
+  const [allCheck, setAllCheck] = React.useState(-1);
+
+  React.useEffect(() => {
+    const start = new Date(`${minimumDate.year}/${minimumDate.month}/${minimumDate.day}`);
+    const end = new Date(`${maximumDate.year}/${maximumDate.month}/${maximumDate.day}`);
+    const daysBetween = (end.getTime() - start.getTime()) / (1000 * 3600 * 24);
+    availableDateTime = {};
+    console.log(start);
+    console.log(end);
+    for (let i = 0; i <= daysBetween; i++) {
+      const temp = new Date(start);
+      temp.setDate(temp.getDate()+i);
+      availableDateTime[`${temp.getFullYear()}-${temp.getMonth()+1}-${temp.getDate()}`] =[...allCheckedList];
+    }
+    setSelectedList([...allCheckedList]);
+  }, [allCheck]);
 
   const MenuItem = ({ text, isSelected }) => {
     return <div><div class="time-item">{text}</div><div
@@ -63,7 +85,11 @@ const DateTimeInput = ({ match }) => {
     setSelectFlag(selectFlag * -1);
   }
 
-  useEffect(() => {
+  const allCheckEvent = () => {
+    setAllCheck(allCheck*-1);
+  }
+
+  useEffect(() => { //Time select 했을 때
     let newSelectedList = [...selectedList];
     newSelectedList[selectedTime] *= -1;
     setSelectedList(newSelectedList);
@@ -88,12 +114,11 @@ const DateTimeInput = ({ match }) => {
         availableDay.splice(idx, 1);
       }
     }
-    console.log(availableDateTime);
   }, [selectedTime, selectFlag]);
 
-  useEffect(() => {
+  useEffect(() => { // Day select 했을 때
     if (`${selectedDay.year}-${selectedDay.month}-${selectedDay.day}` in availableDateTime) {
-      setSelectedList(availableDateTime[`${selectedDay.year}-${selectedDay.month}-${selectedDay.day}`])
+      setSelectedList(availableDateTime[`${selectedDay.year}-${selectedDay.month}-${selectedDay.day}`]);
     }
     else {
       setSelectedList(initSelectedList);
@@ -102,6 +127,7 @@ const DateTimeInput = ({ match }) => {
 
 
   return (
+    
     <div>
       <div class="padding">
         <div class="container">
@@ -126,12 +152,11 @@ const DateTimeInput = ({ match }) => {
           </div>
         </div>
         <pre class="font5">가능한 일정을 모두 선택해주세요.</pre>
-        <svg class="all-check" width="107" height="29" viewBox="0 0 107 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div class="all-click" onClick={allCheckEvent}><svg class="all-check" width="107" height="29" viewBox="0 0 107 29" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect x="0.5" y="0.5" width="106" height="28" rx="14" stroke="#5AA6F8" />
           <path d="M29.8695 16.801V14.403H32.8065V9.772H26.0085V14.403H28.9565V16.801H24.9525V17.549H33.8955V16.801H29.8695ZM26.8775 10.487H31.9265V13.688H26.8775V10.487ZM42.8927 12.621H37.2167V10.267H42.7717V9.53H36.2927V13.347H42.8927V12.621ZM44.0147 14.634H35.0717V15.371H39.0647V18.77H39.9667V15.371H44.0147V14.634ZM54.8668 16.328H55.7798V8.958H54.8668V11.268H52.7108V12.016H54.8668V16.328ZM53.4148 13.996C52.0068 13.468 51.0278 12.17 51.0278 10.795V9.541H50.1258V10.762C50.1258 12.258 49.1358 13.677 47.6618 14.205L48.1348 14.92C49.2898 14.447 50.1588 13.534 50.5878 12.346C51.0168 13.413 51.8418 14.26 52.9418 14.689L53.4148 13.996ZM50.3348 17.857V15.514H49.4328V18.594H56.0108V17.857H50.3348ZM65.2279 8.958V11.521H63.9079V9.134H63.0499V14.865H63.9079V12.258H65.2279V14.931H66.0969V8.958H65.2279ZM62.3899 13.413C61.3669 13.622 60.4759 13.677 59.0679 13.677V12.335H61.8949V11.642H59.0679V10.399H62.0049V9.673H58.1989V14.414H58.8259C60.4099 14.414 61.3339 14.37 62.4779 14.139L62.3899 13.413ZM59.4639 16.174H65.1949V18.814H66.0969V15.448H59.4639V16.174ZM73.741 9.849H71.277V8.848H70.375V9.849H67.922V10.542H73.741V9.849ZM75.545 11.29V8.914H74.643V14.37H75.545V12.038H77.008V11.29H75.545ZM73.191 12.599C73.191 11.587 72.278 10.982 70.826 10.982C69.418 10.982 68.472 11.587 68.472 12.599C68.472 13.611 69.418 14.216 70.826 14.216C72.278 14.216 73.191 13.611 73.191 12.599ZM69.33 12.599C69.33 11.983 69.902 11.653 70.826 11.653C71.75 11.653 72.333 11.983 72.333 12.599C72.333 13.215 71.75 13.545 70.826 13.545C69.902 13.545 69.33 13.215 69.33 12.599ZM70.188 17.065H75.545V14.876H69.275V15.547H74.654V16.405H69.308V18.704H75.908V18.011H70.188V17.065ZM85.5212 8.947V12.841H84.1242V9.167H83.2772V18.308H84.1242V13.578H85.5212V18.803H86.3902V8.947H85.5212ZM79.2072 13.49H82.0122V10.025H78.3052V10.762H81.1322V12.753H78.3382V16.449H78.9872C80.2082 16.449 81.3192 16.405 82.6832 16.174L82.6172 15.426C81.3522 15.646 80.3402 15.69 79.2072 15.701V13.49ZM94.8703 16.801V14.128C95.7063 13.699 96.1683 12.995 96.1683 12.104C96.1683 10.575 94.6833 9.574 92.5823 9.574C90.4923 9.574 88.9963 10.575 88.9963 12.104C88.9963 12.995 89.4803 13.71 90.3163 14.139V16.801H88.1163V17.549H97.0813V16.801H94.8703ZM92.5823 10.278C94.1663 10.278 95.3103 11.004 95.3103 12.104C95.3103 13.204 94.1663 13.93 92.5823 13.93C90.9983 13.93 89.8653 13.204 89.8653 12.104C89.8653 11.004 90.9983 10.278 92.5823 10.278ZM91.2183 14.48C91.6473 14.579 92.0983 14.634 92.5823 14.634C93.0333 14.634 93.4953 14.579 93.9683 14.48V16.801H91.2183V14.48Z" fill="#5AA6F8" />
           <path fill-rule="evenodd" clip-rule="evenodd" d="M13.0465 17.4999C12.9042 17.4999 12.768 17.4334 12.6705 17.3162L10.162 14.2951C9.96646 14.0606 9.97729 13.6913 10.1847 13.4708C10.3925 13.2503 10.7191 13.262 10.9135 13.4965L13.0414 16.0579L17.3786 10.6901C17.5715 10.4515 17.8975 10.4358 18.108 10.6528C18.3179 10.8698 18.3324 11.239 18.1405 11.4764L13.4272 17.3098C13.3308 17.4299 13.1936 17.4988 13.0502 17.4999H13.0465Z" fill="#5AA6F8" />
-        </svg>
-
+        </svg></div>
         <div class="calendar-margin">
           <Calendar
             value={selectedDay}
@@ -152,6 +177,7 @@ const DateTimeInput = ({ match }) => {
           inertiaScrolling={true}
           inertiaScrollingSlowdown={0.7}
           alignCenter={true}
+          translate={TimePickerTranslate}
         />
         <svg class="guide" width="221" height="14" viewBox="0 0 221 14" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M22.799 4.016H20.643V3.268H22.799V0.958H23.712V8.328H22.799V4.016ZM20.874 6.689C20.324 6.47633 19.8473 6.16833 19.444 5.765C19.0407 5.35433 18.7327 4.88133 18.52 4.346C18.3073 4.94 17.9883 5.46067 17.563 5.908C17.145 6.348 16.6463 6.68533 16.067 6.92L15.594 6.205C15.9607 6.073 16.2943 5.88967 16.595 5.655C16.903 5.42033 17.1633 5.15267 17.376 4.852C17.596 4.544 17.7647 4.214 17.882 3.862C17.9993 3.50267 18.058 3.136 18.058 2.762V1.541H18.96V2.795C18.96 3.13967 19.0187 3.477 19.136 3.807C19.2533 4.12967 19.4147 4.434 19.62 4.72C19.8327 4.99867 20.0857 5.248 20.379 5.468C20.6723 5.688 20.995 5.864 21.347 5.996L20.874 6.689ZM23.943 9.857V10.594H17.365V7.514H18.267V9.857H23.943ZM34.0291 0.958V6.931H33.1601V4.258H31.8401V6.865H30.9821V1.134H31.8401V3.521H33.1601V0.958H34.0291ZM30.4101 6.139C30.1241 6.19767 29.8455 6.24533 29.5741 6.282C29.3101 6.31867 29.0351 6.348 28.7491 6.37C28.4631 6.38467 28.1588 6.39567 27.8361 6.403C27.5135 6.41033 27.1541 6.414 26.7581 6.414H26.1311V1.673H29.9371V2.399H27.0001V3.642H29.8271V4.335H27.0001V5.677C27.3521 5.677 27.6748 5.67333 27.9681 5.666C28.2615 5.65867 28.5401 5.644 28.8041 5.622C29.0681 5.6 29.3211 5.57433 29.5631 5.545C29.8125 5.50833 30.0655 5.46433 30.3221 5.413L30.4101 6.139ZM27.3961 7.448H34.0291V10.814H33.1271V8.174H27.3961V7.448ZM43.5763 4.357H36.9983V1.101H37.8893V2.036H42.6853V1.101H43.5763V4.357ZM42.6853 2.718H37.8893V3.664H42.6853V2.718ZM40.7273 5.886V6.854H43.5983V9.054H37.8013V10.011H43.8953V10.704H36.9213V8.394H42.7073V7.536H36.8883V6.854H39.8253V5.886H35.8103V5.16H44.7423V5.886H40.7273ZM55.1144 5.699H53.5304V10.814H52.6284V0.947H53.5304V4.951H55.1144V5.699ZM50.9784 2.003C50.9784 2.707 50.8941 3.389 50.7254 4.049C50.5568 4.709 50.2928 5.336 49.9334 5.93C49.5741 6.524 49.1121 7.07767 48.5474 7.591C47.9828 8.097 47.3008 8.548 46.5014 8.944L46.0064 8.24C47.2971 7.59467 48.2724 6.81733 48.9324 5.908C49.5998 4.99867 49.9848 3.94633 50.0874 2.751H46.4684V2.003H50.9784Z" fill="#8A8A8A" />
