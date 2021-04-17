7d216b6642603bd74145c8b586c5360b5118f49c
unknown <lgun202@naver.com>
2021-04-18 01:12:39 +0900

    [feat] 앱스토어로 이동

--- src/commons/components/Modals/Modal.js
+++ src/commons/components/Modals/Modal.js
@@ -13,10 +13,10 @@ const Modal = (props) => {
                     <header>
                     </header>
                     <main>
-                        <img  src="/teampang_info2.png" />
+                        <img src="/teampang_info2.png" />
                     </main>
                     <footer>
-                        <button className="button1" onClick={close}> 팀프앙 앱으로 보기 </button>
+                        <a href={"https://apps.apple.com/kr/app/%ED%8C%80%ED%94%84%EC%95%99/id1563146145"}><button className="button1" onClick={close}> 팀프앙 앱으로 보기 </button></a>
                         <button className="button2" onClick={close}> 모바일 웹으로 볼래요 </button>
                     </footer>
                 </section>
